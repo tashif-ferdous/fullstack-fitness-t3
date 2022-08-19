2610b2e17a01e2214c0a515dd3914ad4029c7a27
Tashif Ferdous <ferdoustashif@gmail.com>
2022-08-19 14:14:29 -0400

    Set up the sets, exercise, and program seed for the db.

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,7 +4,7 @@
 const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 
-async function seedLift() {
+async function seedLifts() {
   const deadlift = await prisma.lift.upsert({
     where: {
       name: 'Barbell Sumo Deadlift'
@@ -33,13 +33,139 @@ async function seedLift() {
     },
   })
 
+  const bench = await prisma.lift.upsert({
+    where: {
+      name: 'Barbell Flat Bench Press'
+    },
+    update: {},
+    create: {
+      name: 'Barbell Flat Bench Press',
+      movement: {
+        create: {
+          name: 'Flat Bench'
+        }
+      },
+      equipment: {
+        connect: {
+          name: 'Barbell'
+        }
+      },
+      muscles: {
+        connect: [
+          { name: 'Lats' }
+        ],
+        create: [
+          { name: 'Pecs' },
+          { name: 'Triceps' }
+        ]
+      }
+    }
+  })
+
   console.log(deadlift)
-  return deadlift
+  console.log(bench)
 }
 
-seedLift()
+async function seedSets() {
+  const upsertBenchSet = async (rep) => {
+    return await prisma.set.upsert({
+      where: { name: `BB Flat Bench Press ${rep} reps` },
+      update: {},
+      create: {
+        name: `BB Flat Bench Press ${rep} reps`,
+        lift: {
+          connect: { name: 'Barbell Flat Bench Press' }
+        },
+        rep: rep,
+      }
+    })
+  }
+
+  const upsertDeadliftSet = async (rep) => {
+    return await prisma.set.upsert({
+      where: { name: `BB Sumo Deadlift ${rep} reps` },
+      update: {},
+      create: {
+        name: `BB Sumo Deadlift ${rep} reps`,
+        lift: {
+          connect: { name: 'Barbell Sumo Deadlift' }
+        },
+        rep: rep,
+      }
+    })
+  }
+
+  const reps = [2, 4, 6, 8, 10, 12, 15]
+  const benchReps = []
+  const deadliftReps = []
+  for (const rep of reps) {
+    const benchSet = await upsertBenchSet(rep)
+    const deadliftSet = await upsertDeadliftSet(rep)
+    benchReps.push(benchSet)
+    deadliftReps.push(deadliftSet)
+    console.log(benchSet)
+    console.log(deadliftSet)
+  }
+
+  return { benchReps, deadliftReps }
+}
+
+async function seedExercise({ benchReps, deadliftReps }) {
+  const deadliftSetIds = deadliftReps.map(set => {
+    return set.id
+  })
+  const benchSetIds = benchReps.map(set => {
+    return set.id
+  })
+  const deadlift = await prisma.exercise.upsert({
+    where: {
+      name: 'BB Sumo Deadlifts'
+    },
+    update: {},
+    create: {
+      name: 'BB Sumo Deadlifts',
+      sets: {
+        connect: deadliftSetIds.map(id => { return { id: id } })
+      },
+      orderSetIds: deadliftSetIds,
+      lift: {
+        connect: {
+          name: 'Barbell Sumo Deadlift'
+        }
+      }
+    }
+  })
+  const bench = await prisma.exercise.upsert({
+    where: {
+      name: 'BB Flat Bench Press'
+    },
+    update: {},
+    create: {
+      name: 'BB Flat Bench Press',
+      sets: {
+        connect: benchSetIds.map(id => { return { id: id } })
+      },
+      orderSetIds: benchSetIds,
+      lift: {
+        connect: {
+          name: 'Barbell Flat Bench Press'
+        }
+      }
+    }
+  })
+
+  console.log(bench)
+  console.log(deadlift)
+  return { bench, deadlift }
+}
+
+seedLifts()
   .then(async () => {
-    await prisma.$disconnect()
+    seedSets().then(async ({ benchReps, deadliftReps }) => {
+      seedExercise({ benchReps, deadliftReps }).then(async ({ bench, deadlift }) => {
+        await prisma.$disconnect()
+      })
+    })
   })
   .catch(async (e) => {
     console.error(e)
